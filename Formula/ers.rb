41131aca72ebd23cdee2b5ec3581e73bddca7737
espadonne <espadonne@outlook.com>
2026-03-21 23:30:15 -0400

    ers 0.1.2

--- Formula/ers.rb
+++ Formula/ers.rb
@@ -1,8 +1,8 @@
 class Ers < Formula
   desc "Window border renderer for macOS"
   homepage "https://github.com/gardesk/ers"
-  url "https://github.com/gardesk/ers/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "85e4c82b571c456d87eedbf044ce29e031614f002c31fcbe182e697f7027f208"
+  url "https://github.com/gardesk/ers/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "650c02a90ea32ac3f18586848847b61e0a8a0f61a0f5e0b9aafbe6e79b85ddbe"
   license "MIT"
 
   depends_on "rust" => :build
