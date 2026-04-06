a7e8493b8d7ba50212e2db16e944c277993e9b8c
espadonne <espadonne@outlook.com>
2026-04-06 02:09:04 -0400

    bump tarmac to v0.2.1: fix startup panic on some systems

--- Formula/tarmac.rb
+++ Formula/tarmac.rb
@@ -1,9 +1,9 @@
 class Tarmac < Formula
   desc "Tiling window manager for macOS, configured with Lua"
   homepage "https://github.com/gardesk/tarmac"
-  url "https://github.com/gardesk/tarmac/releases/download/v0.1.5/tarmac-v0.1.5-darwin-arm64.tar.gz"
-  sha256 "31762e5bd32033c9940f2c44d2d7963c37e439a5bf9ff367e30775e587b28133"
-  version "0.1.5"
+  url "https://github.com/gardesk/tarmac/releases/download/v0.2.1/tarmac-v0.2.1-darwin-arm64.tar.gz"
+  sha256 "64f540715760be1a9a05164fa8a27f0c79c7f6a93b8da6b009c4fd2bc7db4c25"
+  version "0.2.1"
   license "MIT"
 
   depends_on :macos
