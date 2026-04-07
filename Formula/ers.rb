6efa3a6ba23dd77ab32299d3d216449450b774c9
espadonne <espadonne@outlook.com>
2026-04-07 18:24:22 -0400

    bump ers to 0.3.0

--- Formula/ers.rb
+++ Formula/ers.rb
@@ -1,8 +1,8 @@
 class Ers < Formula
   desc "Window border renderer for macOS"
   homepage "https://github.com/gardesk/ers"
-  url "https://github.com/gardesk/ers/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "7c086810c7d198f221d28d0a1438ff5b3750e99de0d560259046b1a168b891bc"
+  url "https://github.com/gardesk/ers/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "d0a3afb745415130836558bf753b0fce0d6263ae37ee61423b03ddc22024fc47"
   license "MIT"
 
   depends_on "rust" => :build
