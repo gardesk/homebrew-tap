020e0a11f50a092ff5d0e2c2c262f2ec0290a0f7
mfwolffe <wolffemf@dukes.jmu.edu>
2026-05-04 16:34:29 -0400

    tarmac 0.3.7

--- Formula/tarmac.rb
+++ Formula/tarmac.rb
@@ -1,9 +1,9 @@
 class Tarmac < Formula
   desc "Tiling window manager for macOS, configured with Lua"
   homepage "https://github.com/gardesk/tarmac"
-  url "https://github.com/gardesk/tarmac/releases/download/v0.3.6/tarmac-v0.3.6-darwin-arm64.tar.gz"
-  sha256 "21ad45acd24ff97e3ad19d93df56ed4713e1766700b33fc8fb41588cf7ebc817"
-  version "0.3.6"
+  url "https://github.com/gardesk/tarmac/releases/download/v0.3.7/tarmac-v0.3.7-darwin-arm64.tar.gz"
+  sha256 "03851a2a25929a946cde4082f608a1dfe593fedc78a76549f120525ba408daaa"
+  version "0.3.7"
   license "MIT"
 
   depends_on :macos
